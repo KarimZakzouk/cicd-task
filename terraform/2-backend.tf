92674c138816c372f9ba28b4b2a144a1fd036152
KarimZakzouk <karimzakzouk69@gmail.com>
2025-08-21 20:06:29 +0300

    Update EKS kubeconfig cluster name and adjust Terraform backend configuration

--- terraform/2-backend.tf
+++ terraform/2-backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "solar-system-terraform-state-123456"
+    bucket = "karim-farid-terraform-state"
     key    = "o-tel-demo/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "solar-system-terraform-locks"
+    dynamodb_table = "terraform-lock-table"
     encrypt = true
   }
 }
